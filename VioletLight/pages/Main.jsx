bb044186131684d89ff1fff2c1e8f02583fff061
Tiana Aldroubi <tianama02@gmail.com>
2022-02-05 12:53:56 -0500

    made different page views

--- VioletLight/pages/Main.jsx
+++ VioletLight/pages/Main.jsx
@@ -1,8 +1,8 @@
 import * as Battery from 'expo-battery';
-import { SafeAreaView, View, Text, TextInput, Button } from 'react-native';
+import { SafeAreaView, View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import { useState } from 'react';
 
-const Main = () => {
+const Main = ( {route, navigation } : any) => {
     const [batteryLevel, setBatteryLevel] = useState(null);
     const [text, setText] = useState('');
 
@@ -10,18 +10,74 @@ const Main = () => {
         const { level } = await Battery.getBatteryLevelAsync();
         setBatteryLevel(level);
     }
+    const [view, setView] = useState("A");
 
     return (
+        <>
         <SafeAreaView>
             <View>
-                <Text>Battery Level: {batteryLevel}</Text>
+                {/* The button which will take you to the page to create a group where you will input your name and then the distance */}
+                
+                {/* This button will take you to a page where you  can enter the party code or maybe scan a QR code*/}
+                
+                {/* <Text>Battery Level: {batteryLevel}</Text>
                 <TextInput onChangeText={setText} value={text} />
-                <Button title="send" onPress={getBatteryLevel} />
+                <Button title="send" onPress={getBatteryLevel} /> */}
+            </View>
+            <View style={styles.container}>
+                {view === "A" && (
+                <>
+                    <Text>Default</Text>
+                    <Button title='Start' onPress={() => setView("B")} title="Start"/>
+                </>
+                
+                )}
+                {view === "B" && (
+                <>
+                    <Text>Home</Text>
+                    <Button title='Create Group' onPress={() => setView("C")} title="Create Group"/>
+                    <Button title='Join Group' onPress={() => setView("D")} title="Join Group"/>
+                    <Button title='Back' onPress={() => setView("A")} title="Back"/>
+                </>
+                )}
+                {view === "C" && (
+                <>
+                    <Text>Create group</Text>
+                    <Text>enter name</Text>
+                    <Text>enter distance</Text>
+                    <Button title='Done' onPress={() => setView("E")} title="Start"/>
+                </>
+                
+                )}
+                {view === "D" && (
+                <>
+                    <Text>Join group</Text>
+                    <Text>enter ID</Text>
+                    <Button title='Done' onPress={() => setView("E")} title="Start"/>
+                </>
+                
+
+                )}
+                {view === "E" && (
+                    <>
+                        <Text>group view</Text>
+                        <Button title='End party' onPress={() => setView("A")} title="Done"/>
+                    </>
+                
+                )}
             </View>
         </SafeAreaView>
+        </>
     );
     
 
-}
-
+};
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#AAA',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
 export default Main;
