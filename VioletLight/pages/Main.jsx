4aca3b475bba4000fb7f0d41baa1c2df558bcf29
Ayon Bhowmick <ayb224@lehigh.edu>
2022-02-05 23:32:48 -0500

    get stuff

--- VioletLight/pages/Main.jsx
+++ VioletLight/pages/Main.jsx
@@ -32,6 +32,9 @@ const Main = () => {
     const makeGroup = async () => {
         const bat = await getBatteryLevel();
         const loc = await getLocation();
+        await fetch("http://128.180.206.51:3000/api/update", {
+            body: JSON.stringify({"device": Device.deviceName, "name": name, "distance": "0", "battery": bat, "number": phone})
+        });
         await fetch("http://128.180.206.51:3000/api/makeGroup", {
             body: JSON.stringify({"distance": distance, "location": loc}),
             method: "POST",
