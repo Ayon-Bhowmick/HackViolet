be486b5044838e4e208a2598bb6a967d6a59d4bf
Tiana Aldroubi <tianama02@gmail.com>
2022-02-05 22:54:20 -0500

    distance

--- VioletLight/pages/List.jsx
+++ VioletLight/pages/List.jsx
@@ -6,15 +6,15 @@ export default App = () => {
 
 
 
-const alertFunction = async () =>{
-  console.log("ALERT ACTIVATED")
+  const alertFunction = async () => {
+    console.log("ALERT ACTIVATED")
     // ERICK CODE HERE
-}
+  }
 
-const generateID = async () => {
+  const generateID = async () => {
 
-  let ID = Math.floor(Math.random() * 1001);
-  
+    let ID = Math.floor(Math.random() * 1001);
+
     //Generate a random number XXXX
     //compare this generated number with all ID's currently in the database
     //if this number matches any one of them generate a new random number and redo
@@ -22,59 +22,67 @@ const generateID = async () => {
     //create a new group on server with this meeting ID and display
 
     return ID;
-}
+  }
 
 
-const checkBatteryLocation = async () => {
+  const checkBatteryLocation = async () => {
 
     //check the current battery of the device
     let battery = await getBatteryLevelAsync();
-    let percentage = Math.round(battery *100);
+    let percentage = Math.round(battery * 100);
     console.log(percentage);
-    
-    //check the current location
 
 
+    //get location
+    const location = await Location.getCurrentPositionAync();
+    let cord = [ locatoin.coords.latitude, location.coords.longitude]
+
+  
     //send this information to the server
-}
+    //find the user based on their device info
+    //to a put request to server to update battery percentage
 
-useEffect(() => {
-  checkBatteryLocation()
-  return () => clearInterval(interval);
- }, []);
+    
 
+  }
 
-setInterval(checkBatteryLocation, 60000);
-
-//this function checks the location and battery of all friends
-async function checkFriends() {
-
-  // await fetch("http://128.180.206.51:3000/api/makeGroup", {
+  useEffect(() => {
+    checkBatteryLocation()
+    return () => clearInterval(interval);
+  }, []);
 
 
-  // })
+  setInterval(checkBatteryLocation, 60000);
 
-  //for loop to go through everyones location
-
-  
-  
-}
-
-useEffect(() => {
-  checkFriends()
-
-  return () => clearInterval(interval);
- }, []);
+  //this function checks the location and battery of all friends
+  async function checkFriends() {
 
 
-setInterval(checkFriends, 300000);
+
+
+    //TODO integrate this functionally into the app
+    let distance = Math.sqrt(Math.pow((currentlocation[0] - friendlocation[0]), 2) + Math.pow((currentlocation[1] - friendlocation[1]), 2))
+    //for loop to go through everyones location
+
+
+
+  }
+
+  useEffect(() => {
+    checkFriends()
+
+    return () => clearInterval(interval);
+  }, []);
+
+
+  setInterval(checkFriends, 300000);
 
 
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
 
   const getMovies = async () => {
-     try {
+    try {
       const response = await fetch('https://reactnative.dev/movies.json');
       const json = await response.json();
       setData(json.movies);
@@ -91,7 +99,7 @@ setInterval(checkFriends, 300000);
 
   return (
     <View style={{ flex: 1, padding: 24 }}>
-      {isLoading ? <ActivityIndicator/> : (
+      {isLoading ? <ActivityIndicator /> : (
         <FlatList
           data={data}
           keyExtractor={({ id }, index) => id}
@@ -103,13 +111,13 @@ setInterval(checkFriends, 300000);
 
 
 
-  <Button
+      <Button
         color="red"
         title="Emergency"
         // onPress={() => alert('Button Tapped')} // generic alert
         onPress={() => Alert.alert("Are you sure you want to send alert?", " ", [
-          {text: "No", onPress: () => console.log("No")},
-          {text: "Yes", onPress: () => {alertFunction()}},
+          { text: "No", onPress: () => console.log("No") },
+          { text: "Yes", onPress: () => { alertFunction() } },
         ])}
 
       />
