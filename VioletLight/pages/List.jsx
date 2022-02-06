a37945cab678a608961d91a42db5d880b2b1d791
Josh Tripoli <jct324@lehigh.edu>
2022-02-05 19:00:41 -0500

    Updated List

--- VioletLight/pages/List.jsx
+++ VioletLight/pages/List.jsx
@@ -1,108 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, FlatList, Platform, SafeAreaProvider} from "react-native";
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
 
-const userList = [
-  {id: 1, name: 'Black Panter'},
-  {id: 2, name: 'Black Widow'},
-  {id: 3, name: 'Captain America'},
-  {id: 4, name: 'The Collector'},
-  ];
+export default App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
 
-  const myKeyExtractor = (item) => {
-    return item.id
+  const getMovies = async () => {
+     try {
+      const response = await fetch('https://reactnative.dev/movies.json');
+      const json = await response.json();
+      setData(json.movies);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
   }
-  
-  const renderItem = ({item}) => {
-    return <View><Text>{item.name}</Text></View>
-  }
-  
-  const Header = () => {
-    return (
-      <View style={styles.header}>
-        <Text style={styles.headerText}>
-          Marvel list
-        </Text>
-      </View>
-    )
-  }
-  
-  const Footer = () => {
-    return (
-      <View style={styles.footer}>
-        <Text style={styles.footerText}>
-          Thanks for reading!
-        </Text>
-      </View>
-    )
-  }
-  
-  const List = () => {
-      const [refreshing, setRefreshing] = React.useState(false)
-  
-      const handleRefresh = () => {
-        setRefreshing(prevState => !prevState)
-      }
-  
-        return (
-          <SafeAreaView style={styles.separateHero}>
-            <Header />
-            <FlatList
-              data={userList}
-              renderItem={renderItem}
-              ItemSeparatorComponent={
-                Platform.OS !== 'android' &&
-                 (({ highlighted }) => (
-                   <View
-                      style={
-                         styles.separator
-               }
-                  />
-                ))
-              }
-              keyExtractor={myKeyExtractor}
-              refreshing={refreshing}
-              onRefresh={handleRefresh}
-            />
-            <Footer />
-          </SafeAreaView>
-          )
-      }
-  
-  const styles = StyleSheet.create({
-    separator: {
-      height: 1,
-      width: "100%",
-      backgroundColor: '#ff0000',
-    },
-    separateHero: {
-      height: '100vh' 
-  },
-      header: {
-        backgroundColor: 'red',
-        width: '100vw',
-        height: 45,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginBottom: 20
-      },
-      headerText: {
-        color: '#fff',
-        fontSize: 18
-      },
-      footer: {
-        backgroundColor: 'white',
-        width: '100vw',
-        height: 45,
-        justifyContent: 'center',
-        alignItems: 'center',
-        bottom: 0
-      },
-      footerText: {
-        color: '#000',
-        fontSize: 18
-      }
-  });
-  
-  
-  export default List
-  
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>{item.title}, {item.releaseYear}</Text>
+          )}
+        />
+      )}
+    </View>
+  );
+};
