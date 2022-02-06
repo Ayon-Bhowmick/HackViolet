1954a2a1b80ad787805a30334fa0e56dc6dc0aeb
Josh Tripoli <jct324@lehigh.edu>
2022-02-06 00:00:19 -0500

    Got List Working With Database! Prints out everything

--- VioletLight/pages/List.jsx
+++ VioletLight/pages/List.jsx
@@ -1,79 +1,15 @@
-import { getBatteryLevelAsync } from 'expo-battery';
 import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, FlatList, Button, Text, View, Alert } from 'react-native';
+import { ActivityIndicator, StyleSheet, FlatList, Text, View } from 'react-native';
 
-export default App = () => {
-
-
-
-const alertFunction = async () =>{
-  console.log("ALERT ACTIVATED")
-    // ERICK CODE HERE
-}
-
-const generateID = async () => {
-    //Generate a random number XXXX
-    //compare this generated number with all ID's currently in the database
-    //if this number matches any one of them generate a new random number and redo
-    //if this number does not match any of them then set this number to be the meeting ID 
-    //create a new group on server with this meeting ID and display
-    
-}
-
-
-const checkBatteryLocation = async () => {
-
-    //check the current battery of the device
-    let battery = await getBatteryLevelAsync();
-    let percentage = Math.round(battery *100);
-    console.log(percentage);
-    
-    //check the current location
-
-
-    //send this information to the server
-}
-
-useEffect(() => {
-  checkBatteryLocation()
-  return () => clearInterval(interval);
- }, []);
-
-
-setInterval(checkBatteryLocation, 60000);
-
-//this function checks the location and battery of all friends
-async function checkFriends() {
-
-  // await fetch("http://128.180.206.51:3000/api/makeGroup", {
-
-
-  // })
-
-  //for loop to go through everyones location
-
-  
-  
-}
-
-useEffect(() => {
-  checkFriends()
-
-  return () => clearInterval(interval);
- }, []);
-
-
-setInterval(checkFriends, 300000);
-
-
+const List = () => {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
 
-  const getMovies = async () => {
+  const getUsers = async () => {
      try {
-      const response = await fetch('https://reactnative.dev/movies.json');
+      const response = await fetch('http://128.180.206.51:3000/api/getEveryone');
       const json = await response.json();
-      setData(json.movies);
+      setData(json);
     } catch (error) {
       console.error(error);
     } finally {
@@ -82,7 +18,7 @@ setInterval(checkFriends, 300000);
   }
 
   useEffect(() => {
-    getMovies();
+    getUsers();
   }, []);
 
   return (
@@ -92,24 +28,27 @@ setInterval(checkFriends, 300000);
           data={data}
           keyExtractor={({ id }, index) => id}
           renderItem={({ item }) => (
-            <Text>{item.title}, {item.releaseYear}</Text>
+            <Text style={styles.items}>{item.name}   {item.distance}   {item.battery}</Text>
           )}
         />
       )}
-
-
-
-  <Button
-        color="red"
-        title="Emergency"
-        // onPress={() => alert('Button Tapped')} // generic alert
-        onPress={() => Alert.alert("Are you sure you want to send alert?", " ", [
-          {text: "No", onPress: () => console.log("No")},
-          {text: "Yes", onPress: () => {alertFunction()}},
-        ])}
-
-      />
-
     </View>
+
+    
   );
+
+  
 };
+
+const styles = StyleSheet.create({
+  items: { 
+    backgroundColor:'#FFF',
+    padding: 0,
+    margin: 1,
+    fontSize: 36,
+    height: 44,
+    width: 300,
+    textAlign: 'center',
+  },
+});
+export default List;
